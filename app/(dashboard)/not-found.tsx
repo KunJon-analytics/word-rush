0599e4dbf650b7429ed7968d868488beb9075612
KunJon-analytics <johnadelakun1@gmail.com>
2024-01-13 07:33:11 +0100

    added not found page

--- app/(dashboard)/not-found.tsx
+++ app/(dashboard)/not-found.tsx
@@ -3,7 +3,7 @@ import NotFoundComponent from "@/components/shared/not-found";
 export default function NotFoundPage() {
   return (
     <>
-      <NotFoundComponent backLink="/" backLinkText="Go back Home" />
+      <NotFoundComponent backLink="/" backLinkText="Sign In" />
     </>
   );
 }
---
+++ components/shared/not-found.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import NotFoundImages from "@/components/shared/not-found-images";
+
+interface NotFoundProps {
+  backLink: string;
+  backLinkText: string;
+}
+
+export default function NotFoundComponent({
+  backLink,
+  backLinkText,
+}: NotFoundProps) {
+  return (
+    <section className="h-screen flex items-center justify-center w-full place-content-center px-[var(--margin-x)]">
+      <div className="flex flex-col items-center p-6 text-center">
+        <NotFoundImages />
+        <p className="pt-4 text-xl font-semibold text-slate-800 dark:text-navy-50">
+          Oops. This Page Not Found.
+        </p>
+        <p className="pt-2 text-slate-500 dark:text-navy-200">
+          This page you are looking for is not available. Please go back to home
+        </p>
+        <Button
+          asChild
+          className="btn mt-8 h-11 bg-primary text-base font-medium text-white hover:bg-primary-focus hover:shadow-lg hover:shadow-primary/50 focus:bg-primary-focus focus:shadow-lg focus:shadow-primary/50 active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:hover:shadow-accent/50 dark:focus:bg-accent-focus dark:focus:shadow-accent/50 dark:active:bg-accent/90"
+        >
+          <Link href={backLink}>{backLinkText}</Link>
+        </Button>
+      </div>
+    </section>
+  );
+}
