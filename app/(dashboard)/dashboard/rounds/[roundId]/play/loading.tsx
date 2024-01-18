1ac5775a4cd6a9495cf3fd80e1a3a5b8c3183abb
KunJon-analytics <johnadelakun1@gmail.com>
2024-01-18 15:31:03 +0100

    added loader

--- app/(dashboard)/dashboard/rounds/[roundId]/play/loading.tsx
+++ app/(dashboard)/dashboard/rounds/[roundId]/play/loading.tsx
@@ -4,7 +4,7 @@ import Preloader from "@/components/shared/preloader";
 
 const Loading = () => {
   return (
-    <div className="h-full w-full">
+    <div className="h-screen flex items-center justify-center">
       <Preloader />
     </div>
   );
